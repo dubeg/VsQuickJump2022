69e734e435fe756efbe8ce89cd23bbca33fab773
Guillaume Dube <guillaumedube@hotmail.com>
2025-08-10 17:06:46 -0400

    Add toolwindowpane implementation

--- QuickJump.Tools/DocumentExtensions.cs
+++ QuickJump.Tools/DocumentExtensions.cs
@@ -5,22 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Shapes;
 using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
 using QuickJump2022.Tools;
 
 namespace QuickJump2022.QuickJump.Tools;
 
 public static class DocumentExtensions {
 
-    public static void GoToLine(this ProjectItem projectItem, int lineNo) {
+    public static void GoToLine(this ProjectItem projectItem, int lineNo, bool commit = false) {
         ThreadHelper.ThrowIfNotOnUIThread();
-        var fullPath = projectItem.TryGetProperty<string>("FullPath");
-        if (string.IsNullOrEmpty(fullPath)) return; 
-        QuickJumpData.Instance.Dte.ItemOperations.OpenFile(fullPath, "{00000000-0000-0000-0000-000000000000}");
-        var document = projectItem.Document;
-        document.GoToLine(lineNo);
+        if (!commit) {
+            projectItem.PreviewFile();
+        }
+        else {
+            var fullPath = projectItem.TryGetProperty<string>("FullPath");
+            if (string.IsNullOrEmpty(fullPath)) return;
+            var window = QuickJumpData.Instance.Dte.ItemOperations.OpenFile(fullPath, "{00000000-0000-0000-0000-000000000000}");
+            var document = projectItem.Document;
+            document.GoToLine(lineNo, commit);
+        }
     }
 
-    public static void GoToLine(this Document document, int lineNo) {
+    public static void GoToLine(this Document document, int lineNo, bool commit = false) {
         ThreadHelper.ThrowIfNotOnUIThread("Go");
         if (document != null) {
             var selection = document.Selection;
@@ -28,6 +35,25 @@ public static class DocumentExtensions {
                 txtSel.GotoLine(lineNo, false);
                 txtSel.StartOfLine((vsStartOfLineOptions)1, false);
             }
+        }
+    }
+
+    public static void PreviewFile(this ProjectItem projectItem) {
+        var filePath = projectItem.TryGetProperty<string>("FullPath");
+        var openDoc = Package.GetGlobalService(typeof(SVsUIShellOpenDocument)) as IVsUIShellOpenDocument;
+        Guid logicalView = VSConstants.LOGVIEWID_Primary;
+        int hr = openDoc.OpenDocumentViaProject(
+            filePath,
+            ref logicalView,
+            out _,
+            out var hierarchy,
+            out var itemid,
+            out var windowFrame
+        );
+        if (windowFrame != null) {
+            // Set preview mode
+            windowFrame.SetProperty((int)__VSFPROPID5.VSFPROPID_IsProvisional, true);
+            windowFrame.ShowNoActivate();
         }
     }
 }
