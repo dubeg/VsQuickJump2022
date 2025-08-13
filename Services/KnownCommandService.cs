fd55a90b977a77b49aa9ef43627c8812302a11f1
Guillaume Dubé <guillaume.dube@industriesnrc.com>
2025-08-13 16:34:13 -0400

    Add note about known commands

---
+++ Services/KnownCommandService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickJump2022.Services;
+
+/// <summary>
+/// Retrieve a list of known commands.
+/// </summary>
+public class KnownCommandService {
+    // --------------------
+    // TODO:
+    // Using Community.VisualStudio.Toolkit.KnownCommands && KnownMonikers,
+    // I could hardcode a dictionary of kvp => (KnownCommand, (ImageMoniker, display name))
+    // and return that.
+    //
+    // To build the list of known commands, I could try using AI.
+    // --------------------
+    // Eg. KnownCommands.Build_BatchBuild
+    // --
+    // public static class KnownCommands
+    //   * public static CommandID Build_BatchBuild
+}
