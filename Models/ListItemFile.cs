8eeb3f84ab23352a692496b69157c1f9d519ff84
Guillaume Dubé <guillaume.dube@industriesnrc.com>
2025-08-14 08:39:28 -0400

    Show file paths' location by reversing its path so make the path easier to read

--- Models/ListItemFile.cs
+++ Models/ListItemFile.cs
@@ -1,15 +1,20 @@
 ﻿using EnvDTE;
+using QuickJump2022.Tools;
 
 namespace QuickJump2022.Models;
 
 public class ListItemFile : ListItemBase {
     private FileItem _item;
+    private string _desc;
 
     public override string Name => _item.FileName;
-    public override string Description => _item.ProjectFolderPath;
+    public override string Description => _desc;
     public string FileExtension => _item.FileExtension;
     public string FilePath => _item.FullPath;
 
-    public static ListItemFile FromFileItem(FileItem item) 
-        => new ListItemFile { _item = item };
+    public static ListItemFile FromFileItem(FileItem item)
+        => new ListItemFile { 
+            _item = item,
+            _desc = item.ProjectFolderPath.ReversePath()
+        };
 }
---
+++ Tools/PathExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace QuickJump2022.Tools;
+
+public static class PathExtensions {
+    /// <summary>
+    /// Reverses a file path by reversing the order of its components and joining them with forward slashes.
+    /// For example: "C:\Folder\Subfolder\File" becomes "File/Subfolder/Folder/C:"
+    /// </summary>
+    /// <param name="path">The path to reverse</param>
+    /// <returns>The reversed path with components joined by forward slashes</returns>
+    public static string ReversePath(this string path) {
+        if (string.IsNullOrWhiteSpace(path)) return path;
+        var parts = path.Split(new[] { '\\', '/' }, StringSplitOptions.RemoveEmptyEntries);
+        var reversedParts = parts.Reverse();
+        return string.Join("/", reversedParts);
+    }
+}
