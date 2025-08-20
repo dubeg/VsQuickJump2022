d92fdde9001649d4fce3e46e45ad519b4ef1b826
Guillaume Dubé <guillaume.dube@industriesnrc.com>
2025-08-20 15:39:29 -0400

    Fix height issue caused by max-visible-items by setting a predefined item height

--- Behaviors/ListViewBehaviors.cs
+++ Behaviors/ListViewBehaviors.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
 
 namespace QuickJump2022.Behaviors;
 
@@ -16,15 +18,11 @@ public static class ListViewBehaviors {
             "MaxVisibleItems",
             typeof(int),
             typeof(ListViewBehaviors),
-            new PropertyMetadata(10, OnMaxVisibleItemsChanged));
+            new PropertyMetadata(10, OnMaxVisibleItemsChanged)
+    );
 
-    public static void SetMaxVisibleItems(DependencyObject obj, int value) {
-        obj.SetValue(MaxVisibleItemsProperty, value);
-    }
-
-    public static int GetMaxVisibleItems(DependencyObject obj) {
-        return (int)obj.GetValue(MaxVisibleItemsProperty);
-    }
+    public static void SetMaxVisibleItems(DependencyObject obj, int value) => obj.SetValue(MaxVisibleItemsProperty, value);
+    public static int GetMaxVisibleItems(DependencyObject obj) => (int)obj.GetValue(MaxVisibleItemsProperty);
 
     private static void OnMaxVisibleItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
         if (d is ListBox listView) {
@@ -38,20 +36,13 @@ public static class ListViewBehaviors {
         if (listView.Items.Count == 0) return;
         var maxVisibleItems = GetMaxVisibleItems(listView);
         if (listView.Items.Count > maxVisibleItems) {
-            var itemHeight = GetItemHeight(listView);
-            if (itemHeight == 0) return;
-            var visibleItems = Math.Min(listView.Items.Count, maxVisibleItems);
-            var totalHeight = visibleItems * itemHeight;
-            listView.MaxHeight = totalHeight + 2; // TODO: why 2?
+            var setters = listView.ItemContainerStyle.Setters.OfType<Setter>();
+            var itemHeight = Convert.ToInt32(setters.FirstOrDefault(s => s.Property == FrameworkElement.HeightProperty)?.Value ?? 0);
+            if (itemHeight > 0) {
+                var visibleItems = Math.Min(listView.Items.Count, 20);
+                var totalHeight = visibleItems * itemHeight;
+                listView.MaxHeight = totalHeight + 2; // TODO: why 2?
+            }
         }
-    }
-
-    private static double GetItemHeight(ListBox listView) {
-        // Wait for containers to be generated
-        if (listView.ItemContainerGenerator.Status != GeneratorStatus.ContainersGenerated)
-            return 0;
-
-        var container = listView.ItemContainerGenerator.ContainerFromIndex(0) as ListBoxItem;
-        return container?.DesiredSize.Height ?? 0;
     }
 }
